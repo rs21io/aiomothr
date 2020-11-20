0ffda626527ad13916e00ad12fa7b1b0080c88c3
James Arnold <james@rs21.io>
2020-11-19 18:42:41 -0700

    Bump to v0.2.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md") as f:
 
 setuptools.setup(
     name="aiomothr",
-    version="0.1.3",
+    version="0.2.0",
     author="James Arnold",
     author_email="james@rs21.io",
     description="Asynchronous client library for interacting with MOTHR",
